afd6b821f72e8216d0e8bfe9ed4ea9f3f4de488e
Waleed A. Afifi <waleedafifich@gmail.com>
2021-06-06 12:43:57 +0300

    html structr, js code, css style

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let userName = prompt('Please enter you name!');
+// userName = 'Waleed';
+console.log(userName);
+
+// alert('Welcome ' + userName + ' to our website');
+
+alert(`You name is ${userName}, welcome to our website`);
+console.log(`You name is ${userName}, welcome to our website`);
+
+const courseNumber = 201;
+// courseNumber = 301;
+
+// let courseNumber = 201;
